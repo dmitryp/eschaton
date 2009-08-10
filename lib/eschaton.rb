51e415991a1bbba649422a1feddcc8bd2d6f6bc0
Guilio Karadanais <karadanais@gmail.com>
2009-08-10 17:54:43 +0200

    Issue #10 fix

--- lib/eschaton.rb
+++ lib/eschaton.rb
@@ -1,5 +1,9 @@
 require "#{File.dirname(__FILE__)}/eschaton/eschaton"
+
+# Added for linux distros and to prevet production errors
 require "#{File.dirname(__FILE__)}/eschaton/script_store"
+require "#{File.dirname(__FILE__)}/eschaton/helpers/readable_attributes"
+require "#{File.dirname(__FILE__)}/eschaton/javascript/javascript_object"
 
 Dir["#{File.dirname(__FILE__)}/eschaton/**/*.rb"].each do |file|
   Eschaton.dependencies.require file
