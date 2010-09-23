bb336395194c05f939936c229d270d5f1e05f711
Guilio Karadanais <karadanais@gmail.com>
2010-09-23 16:35:24 +0200

    Don't user render :update anymore

--- lib/slices/eschaton_kernel/kernel_controller_ext.rb
+++ lib/slices/eschaton_kernel/kernel_controller_ext.rb
@@ -1,9 +1,7 @@
 module KernelControllerExt
 
   def run_javascript(&block)
-    render :update do |script|
-      Eschaton.with_global_script script, &block
-    end
+    render :js => Eschaton.with_global_script(&block)
   end
 
 end
